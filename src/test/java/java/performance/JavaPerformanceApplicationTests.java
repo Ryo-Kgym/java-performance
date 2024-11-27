ada832b0d06ff18c9e2ca1c611faadbf75f3657c
Ryo-Kgym <mraytoh@icloud.com>
2024-11-27 09:32:39 +0900

    java-performance プロジェクトを立ち上げた

---
+++ src/test/java/java/performance/JavaPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package java.performance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPerformanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
